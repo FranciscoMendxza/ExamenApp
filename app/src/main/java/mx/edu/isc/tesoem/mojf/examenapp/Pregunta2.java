e87c168f447a3c6813f1ab7a03fb49ee664d21bd
FranciscoMendxza <mendxza12@gmail.com>
2023-10-11 21:43:23 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/isc/tesoem/mojf/examenapp/Pregunta2.java
@@ -0,0 +1,14 @@
+package mx.edu.isc.tesoem.mojf.examenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pregunta2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pregunta2);
+    }
+}
